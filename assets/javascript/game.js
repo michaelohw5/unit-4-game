cba1fd6c0c53d6d34ff14e446d6ef5860f0f5c0b
mohw <michaelohw5@gmail.com>
2018-11-17 09:30:43 -0800

    users score displayed correctly as buttons pressed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // variables
     var goalNum; //number that user aims for, also displayed
-    var userNum; //users number thats displayed on the screen
+    var userNum = 0; //users number thats displayed on the screen
     var gem1; //first gem
     var gem2; //2nd gem
     var gem3; //3rd gem
@@ -23,18 +23,59 @@ $(document).ready(function () {
         gem4 = randNumGen(1, 12);
     } // works 11.15 10am
 
+    var resetDisplay = function() {
+        var tempScore = $("#score").text();
+        tempScore = parseInt(tempScore);
+        tempScore += userNum;
+        $("#score").text(tempScore);
+    }
+
+    //======click functions======
+    $("#gem1").click(function() {
+        userNum += gem1;
+        resetDisplay();
+        console.log(gem1);
+    });
+
+    $("#gem2").click(function() {
+        userNum += gem2;
+        resetDisplay();
+        console.log(gem2);
+    });
+
+    $("#gem3").click(function() {
+        userNum += gem3;
+        resetDisplay();
+        console.log(gem3);
+    });
+
+    $("#gem4").click(function() {
+        userNum += gem4;
+        resetDisplay();
+        console.log(gem4);
+    });
+    
+    
+    //======click function end======
+    
     //display current goalNum onto the screen
-    var displayGoal = function () {
-        goalText = $("#goal").text();
+    var initializeDisplay = function () {
+        var goalText = $("#goal").text();
+        var scoreText = 0;
         goalText += goalNum;
+        scoreText += parseInt(userNum);
         $("#goal").text(goalText);
+        $("#score").text(scoreText);
     } // seems to work
 
-
-    //play
-    var play = function () {
+    var initGame = function() {
         assignNums();
-        displayGoal();
+        initializeDisplay();
+    }
+    //======play======
+    var play = function () {
+        initGame();
+        console
     }
 
     play();
